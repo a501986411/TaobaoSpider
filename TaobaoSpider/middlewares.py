e7866e3af2caff921caf27f3e229ae57ac55ed03
a501986411 <501986411@qq.com>
2019-11-17 12:00:43 +0800

    修改中间件

--- TaobaoSpider/middlewares.py
+++ TaobaoSpider/middlewares.py
@@ -7,7 +7,7 @@
 
 from scrapy import signals
 from random import choice
-
+import requests
 class TaobaospiderSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
@@ -103,12 +103,13 @@ class TaobaospiderDownloaderMiddleware(object):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 class ProxyMiddleware(object):
-    def process_request(self,request,spider):
+
+    def valVer(self):
         proxys = [
-            {"ip":"58.218.200.253","port":7031,"outip":"183.252.126.66"},
-            {"ip":"58.218.200.253","port":6288,"outip":"103.40.223.204"},
-            {"ip":"58.218.201.114","port":6523,"outip":"223.64.7.107"},
-            {"ip":"58.218.200.253","port":8474,"outip":"122.226.158.132"},
+            {"ip":"58.218.200.253", "port":7031, "outip":"183.252.126.66"},
+            {"ip":"58.218.200.253", "port":6288, "outip":"103.40.223.204"},
+            {"ip":"58.218.201.114", "port":6523, "outip":"223.64.7.107"},
+            {"ip":"58.218.200.253", "port":8474, "outip":"122.226.158.132"},
             {"ip":"58.218.200.253","port":7759,"outip":"112.0.15.220"},
             {"ip":"58.218.201.74","port":8432,"outip":"120.83.116.190"},
             {"ip":"58.218.200.253","port":8243,"outip":"111.41.245.236"},
@@ -126,6 +127,35 @@ class ProxyMiddleware(object):
             {"ip":"58.218.200.214","port":8349,"outip":"223.247.86.228"},
             {"ip":"58.218.200.214","port":5603,"outip":"115.203.83.5"}
         ]
+        badNum = 0
+        goodNum = 0
+        good = []
+        for proxy in proxys:
+            try:
+                proxy_host = proxy
+                protocol = 'https' if 'https' in proxy_host else 'http'
+                proxies = {protocol: proxy_host}
+                response = requests.get('http://www.baidu.com', proxies=proxies, timeout=2)
+                if response.status_code != 200:
+                    badNum += 1
+                    print(proxy_host, 'bad proxy')
+                else:
+                    goodNum += 1
+                    good.append(proxies)
+                    print(proxy_host, 'success proxy')
+            except Exception as e:
+                print(e)
+                # print proxy_host, 'bad proxy'
+                badNum += 1
+                continue
+        print('success proxy num : ', goodNum)
+        print('bad proxy num : ', badNum)
+        return good
+
+
+    def process_request(self,request,spider):
+        proxys = self.valVer()
+        
         proxy = choice(proxys)
         if request.url.startswith("http://"):
             request.meta['proxy']="http://"+ proxy['ip'] + ":" + str(proxy['port'])
