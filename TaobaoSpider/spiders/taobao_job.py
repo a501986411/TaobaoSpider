dc1a96cc55a23dc0244a4fb978bc479cfaacc8f7
a501986411 <501986411@qq.com>
2019-11-18 22:30:32 +0800

    修改查询条件

--- TaobaoSpider/spiders/taobao_job.py
+++ TaobaoSpider/spiders/taobao_job.py
@@ -79,7 +79,7 @@ class TaobaoJobSpider(scrapy.Spider):
         获取需要爬去的url
         :return: url list
         """
-        self.cursor.execute('select goods_id from etb_goods where goods_id <> ""')
+        self.cursor.execute('select goods_id from etb_goods where goods_id <> "" and goods_id in (608017591336, 607732047139, 605662241470, 606559762938)')
         goods_id_list = []
         for row in self.cursor.fetchall():
             goods_id_list.append(row['goods_id'])
