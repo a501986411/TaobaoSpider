4dbfffa023373948dc51b82433e76c84a4167102
chenhailong <hailong.chen@xgo.one>
2019-11-18 13:10:31 +0800

    取消使用代理

--- TaobaoSpider/spiders/taobao_job.py
+++ TaobaoSpider/spiders/taobao_job.py
@@ -29,8 +29,8 @@ class TaobaoJobSpider(scrapy.Spider):
         "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
     ]
     url_prefix = [
-        #"https://world.taobao.com/item/%s.htm",
-          "https://www.taobao.com/list/item-amp/%s.htm"
+        # "https://world.taobao.com/item/%s.htm",
+        "https://www.taobao.com/list/item-amp/%s.htm"
     ]
     handle_httpstatus_list = [404]
     # 定义爬虫名称
@@ -54,15 +54,8 @@ class TaobaoJobSpider(scrapy.Spider):
 
     def parse(self, response):
         try:
-            item = TaobaospiderItem()
-            # item['title'] = self.tradition2simple(response.xpath('//h1/text()').extract_first())
-            # logging.debug(response.xpath('//h1/text()').extract_first())
-            # item['goods_id'] = self.goods_id_url[response.url]
-            # # monthly_sales = response.xpath('//span[@class="salesNum"]/text()').extract_first().split('：')
-            # monthly_sales = response.xpath('//div[@class="sub-title"]/span/text()').extract()[1]
-            # item['monthly_sales'] = monthly_sales
-            # item['cover_img'] = self.get_cover_img(response)
             item = self.get_item_for_list_amp(response)
+            # item = self.get_item_for_word(response)
             next_url = self.get_url()
             if next_url:
                 yield scrapy.Request(next_url,
