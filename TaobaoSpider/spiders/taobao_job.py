c8e1899728752a96bfcd0b857905d2595b968119
chenhailong <hailong.chen@xgo.one>
2019-11-19 12:48:57 +0800

    使用代理

--- TaobaoSpider/spiders/taobao_job.py
+++ TaobaoSpider/spiders/taobao_job.py
@@ -29,11 +29,8 @@ class TaobaoJobSpider(scrapy.Spider):
         "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
     ]
     url_prefix = [
-        # "https://world.taobao.com/item/%s.htm",
-        "https://www.taobao.com/list/item-amp/%s.htm"
-    ]
-    taobao_url_prefix = [
         "https://world.taobao.com/item/%s.htm",
+        # "https://www.taobao.com/list/item-amp/%s.htm"
     ]
     handle_httpstatus_list = [404]
     # 定义爬虫名称
@@ -46,7 +43,6 @@ class TaobaoJobSpider(scrapy.Spider):
     db = ''
     cursor = ''
     goods_id_url = {}
-    goods_type = {}
     def __init__(self):
         super().__init__(scrapy.Spider)
         self.db = pymysql.connect('127.0.0.1', 'root', 'chen19920328', 'easy_taobao')
@@ -58,13 +54,8 @@ class TaobaoJobSpider(scrapy.Spider):
 
     def parse(self, response):
         try:
-            logging.debug(response.xpath('//title/text()').extract_first())
-            now_goods_id = self.goods_id_url[response.url]
-            if self.goods_type[now_goods_id] == 1:
-                item = self.get_item_for_list_amp(response)
-            else:
-                item = self.get_tb_item(response)
-            # item = self.get_item_for_word(response)
+            # item = self.get_item_for_list_amp(response)
+            item = self.get_item_for_word(response)
             next_url = self.get_url()
             if next_url:
                 yield scrapy.Request(next_url,
@@ -92,10 +83,6 @@ class TaobaoJobSpider(scrapy.Spider):
         goods_id_list = []
         for row in self.cursor.fetchall():
             goods_id_list.append(row['goods_id'])
-            if "tmall" in row['detail_url']:
-                self.goods_type[row['goods_id']] = 1
-            else:
-                self.goods_type[row['goods_id']] = 0
         goods_id_list.reverse()
         return goods_id_list
 
@@ -112,13 +99,9 @@ class TaobaoJobSpider(scrapy.Spider):
         next_url = ''
         if len(self.goods_id) > 0:
             next_goods_id = self.goods_id.pop()
-            if self.goods_type[next_goods_id] == 1:
-                next_url = choice(self.url_prefix) % (str(next_goods_id))
-            else:
-                next_url = choice(self.taobao_url_prefix) % (str(next_goods_id))
+            next_url = choice(self.url_prefix) % (str(next_goods_id))
             self.goods_id_url[next_url] = next_goods_id
         return next_url
-
 
     def get_cover_img(self, response):
         res = response.xpath("//script/text()").extract()
